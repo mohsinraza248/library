7d04697661322731d6ffe311658438462f958283
mohsinraza248 <mohsinraza248@gmail.com>
2021-12-23 11:23:45 +0530

    push

---
+++ js/myscript1.js
@@ -0,0 +1,28 @@
+var swiper = new Swiper('.blog-slider', {
+    spaceBetween: 30,
+    effect: 'fade',
+    loop: true,
+    mousewheel: {
+      invert: false,
+    },
+    // autoHeight: true,
+    pagination: {
+      el: '.blog-slider__pagination',
+      clickable: true,
+    }
+  });
+  // JavaScript code
+function search_book() {
+	let input = document.getElementById('searchbar').value
+	input=input.toLowerCase();
+	let x = document.getElementsByClassName('row m-3'); //changed from m-5 to m-3
+	
+	for (i = 0; i < x.length; i++) {
+		if (!x[i].innerHTML.toLowerCase().includes(input)) {
+			x[i].style.display="none";
+		}
+		else {
+			x[i].style.display="list-item";				
+		}
+	}
+}
